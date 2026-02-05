6eb819fbb557a238379b7f333093232722185f4f
Luca <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-02-05 21:49:01 +0000

    Versuch den Fehler mit dem Email senden zu beheben.

--- public/navbar.php
+++ public/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar">
   <div class="logo-container">
-    <a href="index.php"><img src="datenbank/bilder/logo/nationalpark-schwarzwald-logo.png" alt="Logo" class="logo"></a>
+    <a href="index.php"><img src="datenbank/bilder/logo/logo.png" alt="Logo" class="logo"></a>
   </div>
   <ul class="navbar-menu">
     <li><a href="index.php">Startseite</a></li>
     <li><a href="galerie.php">Galerie</a></li>
     <li><a href="timeline.php">Zeitstrahl</a></li>
-    <li><a href="kontakt.php">Kontakt</a></li>
+    <li><a href="contact.php">Kontakt</a></li>
   </ul>
   <div class="navbar-toggle" id="navbar-toggle">
     <span></span>
