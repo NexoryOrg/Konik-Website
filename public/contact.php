fe9246f204153e343b9890ecf3a8852ac48bd631
Luca <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-02-08 20:46:46 +0100

    Testbereich in main (#9)
    * Kleine Farbausbesserungen des Kontaktformulars
    
    * Kleine Design anpassung am Handy, nochmal überprüfen am Pc
    
    * Kontaktformular Design angepasst und fehler behoben.
    
    Dazu emailjs hinzugefügt und jetzt auch möglich den Kontakt zum support für Fragen aufzunehmen.
    
    * Fehler in der Kontakt CSS behoben

--- public/contact.php
+++ public/contact.php
@@ -3,34 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.emailjs.com/dist/email.min.js"></script>
+    <link rel="stylesheet" href="stil/footer.css">
     <link rel="stylesheet" href="stil/contact.css">
     <link rel="stylesheet" href="stil/navbar.css">
-    <link rel="stylesheet" href="stil/footer.css">
 </head>
 <body>
 
     <?php include 'navbar.php'; ?>
 
-    <div class="container">
-    <h2>Kontaktformular</h2>
-    <form id="contactForm" onsubmit="sendEmail(event)">
-        <label for="name">Name:</label>
-        <input type="text" id="name" required>
+    <div class="contact-wrapper">
+        <div class="contact-container">
+        <h2>Kontaktformular</h2>
+        <form id="contactForm">
+            <label for="name">Name:</label>
+            <input type="text" id="name" required>
 
-        <label for="email">E-Mail:</label>
-        <input type="email" id="email" required>
+            <label for="email">E-Mail:</label>
+            <input type="email" id="email" required>
 
-        <label for="message">Nachricht:</label>
-        <textarea id="message" rows="5" required></textarea>
+            <label for="message">Nachricht:</label>
+            <textarea id="message" rows="5" required></textarea>
 
-        <button type="submit">Absenden</button>
-        <div id="status"></div>
-    </form>
+            <button id="contact_button" type="submit">Absenden</button>
+            <div id="status"></div>
+        </form>
+        </div>
     </div>
 
-    <script src="funktionnen/contact.js"></script>
-
     <?php include 'footer.php'; ?>
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
+    <script src="funktionen/contact.js"></script>
 
 </body>
