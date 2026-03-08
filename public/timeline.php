8fce8a59fae47a61ccd3542642190217e61093ca
Luca <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-03-08 11:48:17 +0100

    Optimize output buffering and preload images

--- public/timeline.php
+++ public/timeline.php
@@ -1,6 +1,28 @@
 <?php
+if (!ob_start('ob_gzhandler')) {
+    ob_start();
+}
+header('Vary: Accept-Encoding');
+
+header('Cache-Control: max-age=3600, public');
+header('Expires: ' . gmdate('D, d M Y H:i:s', time()+3600) . ' GMT');
+
 $jsonFile = __DIR__ . '/datenbank/json/timeline.json';
 $events = [];
+
+function minify_output($buffer) {
+    $buffer = preg_replace('/<!--([\s\S]*?)-->/', '', $buffer);
+    $buffer = preg_replace('/\s{2,}/', ' ', $buffer);
+    $buffer = preg_replace('/>\s+</', '><', $buffer);
+    return $buffer;
+}
+register_shutdown_function(function() {
+    if (ob_get_length()) {
+        $contents = ob_get_contents();
+        ob_clean();
+        echo minify_output($contents);
+    }
+});
 
 if (file_exists($jsonFile)) {
     $data = file_get_contents($jsonFile);
@@ -34,10 +56,18 @@ if (file_exists($jsonFile)) {
         <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' https://cdnjs.cloudflare.com; script-src 'self';">
         <title>Timeline</title>
         <link rel="icon" type="image/png" href="datenbank/bilder/logo/logo.png">
+        <link rel="preconnect" href="https://cdnjs.cloudflare.com">
+        <link rel="preconnect" href="https://cdnjs.cloudflare.com">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <link rel="stylesheet" href="stil/timeline.css">
         <link rel="stylesheet" href="!navebar/navbar.css">
         <link rel="stylesheet" href="!footer/footer.css">
+<?php
+if (!empty($events) && !empty($events[0]['image'])) {
+    $img = htmlspecialchars($events[0]['image'], ENT_QUOTES, 'UTF-8');
+    echo "        <link rel=\"preload\" as=\"image\" href=\"{$img}\">\n";
+}
+?>
     </head>
     <body>
 
@@ -52,7 +82,7 @@ if (file_exists($jsonFile)) {
                         <div class="timeline-item <?= $index % 2 == 0 ? 'left' : 'right' ?>">
                             <div class="timeline-date"><?= htmlspecialchars($event['date']) ?></div>
                             <div class="timeline-content">
-                                <img class="timeline-img" src="<?= htmlspecialchars($event['image']) ?>" alt="<?= htmlspecialchars($event['title']) ?>">
+                                <img class="timeline-img" data-src="<?= htmlspecialchars($event['image']) ?>" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==" alt="<?= htmlspecialchars($event['title']) ?>">
                                 <h3 class="timeline-title"><?= htmlspecialchars($event['title']) ?></h3>
                                 <p><?= htmlspecialchars($event['des']) ?></p>
                             </div>
@@ -64,8 +94,8 @@ if (file_exists($jsonFile)) {
 
         <?php include '!footer/footer.php'; ?>
 
-        <script src="funktionen/timeline.js"></script>
-        <script src="!navebar/navbar.js"></script>
+        <script defer src="funktionen/timeline.js"></script>
+        <script defer src="!navebar/navbar.js"></script>
 
     </body>
 </html>
