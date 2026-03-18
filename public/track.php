acd25a99b1decccb7dec18bcb6737fd6c81b0e9b
Luca <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-03-18 18:12:03 +0100

    Update track.php

--- public/track.php
+++ public/track.php
@@ -1,7 +1,8 @@
 <?php
-$file = __DIR__ . "/../datenbank/data/visitors.json";
+$file = __DIR__ . "../datenbank/data/visitors.json";
 
 $data = [];
+
 if (file_exists($file)) {
     $contents = file_get_contents($file);
     $parsed = json_decode($contents, true);
@@ -10,17 +11,25 @@ if (file_exists($file)) {
     }
 }
 
-$hour = date("Y-m-d H:00");
+$currentHour = date("Y-m-d H:00");
 
-if (!isset($data[$hour]) || !is_numeric($data[$hour])) {
-    $data[$hour] = 0;
+if (!isset($data[$currentHour])) {
+    $data[$currentHour] = 0;
 }
 
-$data[$hour]++;
+$data[$currentHour]++;
 
-$data = array_slice($data, -24, 24, true);
+$last7 = [];
+for ($i = 6; $i >= 0; $i--) {
+    $hour = date("Y-m-d H:00", strtotime("-$i hour"));
+    
+    if (!isset($data[$hour])) {
+        $last7[$hour] = 0;
+    } else {
+        $last7[$hour] = $data[$hour];
+    }
+}
 
-file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT), LOCK_EX);
+file_put_contents($file, json_encode($last7, JSON_PRETTY_PRINT), LOCK_EX);
 
 header('Content-Type: application/json; charset=UTF-8');
-echo json_encode(['status' => 'ok', 'hour' => $hour, 'count' => $data[$hour]]);
